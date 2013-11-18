09603b4870c77f0f15e92ce573e97a8279a7a45e
Anthony France <anthony.c.france@gmail.com>
2013-11-17 22:15:02 -0600

    Changes

---
+++ ImageOrder/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageOrder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent( ); 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.Cancel)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                string[] filePaths = Directory.GetFiles(dialog.SelectedPath, "*.jpg");
+                var test = 1;
+            }
+        }
+    }
+}
